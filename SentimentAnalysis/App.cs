38da2cb3140710d627d474f9dddc78f626914be6
Mauricio Contreras <m_contreras@outlook.cl>
2018-11-28 12:54:42 -0300

    App Center SDK integración

--- SentimentAnalysis/App.cs
+++ SentimentAnalysis/App.cs
@@ -1,11 +1,20 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace SentimentAnalysis
 {
     public class App : Application
     {
-        public App() => MainPage = new SentimentPage();
+        public App()
+        {
+            Microsoft.AppCenter.AppCenter.Start("android=491b6faf-4f53-4bc9-bae9-83b0c4e5ca34;" + typeof(Analytics), typeof(Crashes));
+            MainPage = new SentimentPage();
+
+        }
+
+        
     }
 }
